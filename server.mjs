728ce57b2c6ef2f99e222d64f52cdd74d8e9fd6a
talkomatic-donjamie <donjamie@talkomatic.co>
2025-06-05 22:06:16 -0400

    Add files via upload

--- server.mjs
+++ server.mjs
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
 res.sendFile("index.html", { root: "/home/pi/project/site" })
 });
 
+console.log('Starting...');
+setInterval(() => {
+  console.log('Still running...');
+}, 1000);
+
+
 app.listen(9000, () => {
   console.log(`app listening on port 9000`)
 })
